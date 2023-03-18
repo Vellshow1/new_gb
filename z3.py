0a78cf77a0a83972c4bbb3ba1ad8e60b5fc9e842
Valentin <valek099@bk.ru>
2023-03-19 00:24:09 +0300

    задачи 3 урока

--- z3.py
+++ z3.py
@@ -35,20 +35,24 @@ import random
 #         max_max =0
 # print(max)
 
-a = int(input('Введите кол-во арбузов: '))
+# a = int(input('Введите кол-во арбузов: '))
 
-numbers= list()
+# numbers= list()
 
-for i in range(int(a)):
-    numbers.append(random.randint(1, 20))
-print(numbers)
+# for i in range(int(a)):
+#     numbers.append(random.randint(1, 20))
+# print(numbers)
 
-min = numbers[0]
-max = numbers[0]
+# min = numbers[0]
+# max = numbers[0]
 
-for i in range(int(a)):
-    if max < numbers[i]:
-        max = numbers[i]
-    elif min > numbers[i]:
-        min = numbers[i]
-print(f'Минимальный килограм арбуза: {min}, а максимальный {max}')
+# for i in range(int(a)):
+#     if max < numbers[i]:
+#         max = numbers[i]
+#     elif min > numbers[i]:
+#         min = numbers[i]
+# print(f'Минимальный килограм арбуза: {min}, а максимальный {max}')
+
+a = [1,3,4,5,6,7,8]
+c = 2
+print(a[c*-1:])
